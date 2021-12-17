5f8370b3720bec9a504fbba1e11a164bbe50fa2d
LAlex <trewstaples@gmail.com>
2021-12-17 14:41:00 +0300

    Добавит возможность переиспользования в презентере фильм

--- src/presenter/film-presenter.js
+++ src/presenter/film-presenter.js
@@ -1,6 +1,6 @@
 import FilmCardView from '../view/film-card-view.js';
 import FilmPopupView from '../view/film-popup-view.js';
-import { render, renderCard, renderPopup, RenderPosition } from '../utils/render.js';
+import { render, replace, remove, renderCard, renderPopup, RenderPosition } from '../utils/render.js';
 
 export default class FilmPresenter {
   #filmsListComponent = null;
@@ -17,6 +17,9 @@ export default class FilmPresenter {
   init = (film) => {
     this.#film = film;
 
+    const prevFilmCardComponent = this.#filmCardComponent;
+    const prevFilmPopupComponent = this.#filmPopupComponent;
+
     this.#filmCardComponent = new FilmCardView(film);
     this.#filmPopupComponent = new FilmPopupView(film);
 
@@ -26,7 +29,26 @@ export default class FilmPresenter {
       document.removeEventListener('keydown', this.#onEscKeyDown);
     });
 
-    render(this.#filmsListComponent.container, this.#filmCardComponent, RenderPosition.BEFOREEND);
+    if (prevFilmCardComponent === null || prevFilmPopupComponent === null) {
+      render(this.#filmsListComponent.container, this.#filmCardComponent, RenderPosition.BEFOREEND);
+      return;
+    }
+
+    if (this.#filmsListComponent.element.contains(prevFilmCardComponent.element)) {
+      replace(this.#filmCardComponent, prevFilmCardComponent);
+    }
+
+    if (document.body.contains(prevFilmPopupComponent.element)) {
+      replace(this.#filmPopupComponent, prevFilmPopupComponent);
+    }
+
+    remove(prevFilmCardComponent);
+    remove(prevFilmPopupComponent);
+  };
+
+  destroy = () => {
+    remove(this.#filmCardComponent);
+    remove(this.#filmPopupComponent);
   };
 
   #replacePopupToCard = () => {
