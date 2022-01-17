de747fe584f7534af2c45986e63c67ea51f904de
LAlex <trewstaples@gmail.com>
2022-01-17 10:26:15 +0300

    Добавит модель в main.js

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import { generateFilm } from './mock/film.js';
 import { generateFilter } from './mock/filters.js';
 import FilmListPresenter from './presenter/films-list-presenter.js';
+import FilmsModel from './model/films-model.js';
 
 const FILMS_COUNT = 15;
 
@@ -14,8 +15,11 @@ const renderCards = () => {
 const films = renderCards();
 const filters = generateFilter(films);
 
+const filmsModel = new FilmsModel();
+filmsModel.films = films;
+
 const siteHeaderElement = document.querySelector('.header');
 const siteMainElement = document.querySelector('.main');
 
-const filmListPresenter = new FilmListPresenter(siteMainElement, siteHeaderElement);
+const filmListPresenter = new FilmListPresenter(siteMainElement, siteHeaderElement, filmsModel);
 filmListPresenter.init(films, filters);
