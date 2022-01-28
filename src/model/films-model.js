d92fa52462538a12f345159575733ff5566a4914
LAlex <trewstaples@gmail.com>
2022-01-28 14:04:51 +0300

    Повторно включил обновление фильма

--- src/model/films-model.js
+++ src/model/films-model.js
@@ -32,9 +32,14 @@ export default class FilmsModel extends AbstractObservable {
       throw new Error('Cannot update unexisting film');
     }
 
-    this.#films = [...this.#films.slice(0, index), update, ...this.#films.slice(index + 1)];
-
-    this._notify(updateType, update);
+    try {
+      const response = await this.#apiService.updateFilm(update);
+      const updatedFilm = this.#adaptFilmToClient(response);
+      this.#films = [...this.#films.slice(0, index), updatedFilm, ...this.#films.slice(index + 1)];
+      this._notify(updateType, updatedFilm);
+    } catch (err) {
+      throw new Error('Cannot update film');
+    }
   };
 
   #adaptFilmToClient = (film) => {
